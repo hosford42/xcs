6ba6edbe05c782ce5b309c1dd497ae0d3f2406e3
Aaron Hosford <hosford42@gmail.com>
2015-05-07 20:14:16 -0500

    Update xcs.py
    Added reference to original paper

--- xcs.py
+++ xcs.py
@@ -1,8 +1,13 @@
 # -------------------------------------------------------------------------------
 # Name:     xcs.py
 # Purpose:  Implements the XCS (Accuracy-based Classifier System) algorithm,
-#           per the description provided in:
-#           http://opim.wharton.upenn.edu/~sok/papers/b/XCSAlgDesc01202001.pdf
+#           roughly according to the description provided in the paper, "An
+#           Algorithmic Description of XCS," by Martin Butz and Stewart Wilson.
+#
+# Butz, M. and Wilson, S. (2001). An algorithmic description of XCS. In Lanzi, P., 
+#   Stolzmann, W., and Wilson, S., editors, Advances in Learning Classifier Systems:
+#   Proceedings of the Third International Workshop, volume 1996 of Lecture Notes in 
+#   Artificial Intelligence, pages 253–272. Springer-Verlag Berlin Heidelberg.
 #
 # Author:       Aaron Hosford
 #
