6db1114136a5f33ab60af44a34078499b26bf1a3
Aaron Hosford <hosford42@gmail.com>
2015-05-11 19:45:47 -0500

    Improve documentation in _fast_bitstrings.py

--- xcs/_fast_bitstrings.py
+++ xcs/_fast_bitstrings.py
@@ -24,6 +24,8 @@ __all__ = [
     'BitString',
 ]
 
+# If numpy isn't installed, this will raise an ImportError, causing bitstrings.py to fall back on the pure Python
+# implementation defined in _slow_bitstrings.py
 import numpy
 
 
@@ -42,6 +44,12 @@ class BitString:
         """Create a bit string from an integer value. If the length parameter is provided, it determines the number of
         bits in the bit string. Otherwise, the minimum length required to represent the value is used."""
 
+        if not isinstance(value, int):
+            raise TypeError(value)
+
+        # Progressively chop off low-end bits from the int, adding them to the bits list,
+        # until we have reached the given length (if provided) or no more non-zero bits
+        # remain (if length was not specified).
         bits = []
         while value:
             if length is not None and len(bits) >= length:
@@ -49,12 +57,16 @@ class BitString:
             bits.append(value % 2)
             value >>= 1
 
+        # Ensure that if length was provided, we have the correct number of bits in our list.
         if length:
             if len(bits) < length:
                 bits.extend([0] * (length - len(bits)))
             elif len(bits) > length:
                 bits = bits[:length]
 
+        # Reverse the order of the bits, so the high-order bits go on the left and the low-
+        # order bits go on the right, just as a person would expect when looking at the
+        # number written out in binary.
         bits.reverse()
 
         return cls(bits)
@@ -64,27 +76,21 @@ class BitString:
             # noinspection PyUnresolvedReferences
             if bits.flags.writeable:
                 # noinspection PyNoneFunctionAssignment
-                self._bits = bits.copy()
-                self._bits.writeable = False
+                self._bits = bits.copy()  # If it's writable, we need to make a copy
+                self._bits.writeable = False  # Make sure our copy isn't writable
             else:
-                self._bits = bits
+                self._bits = bits  # If it isn't writable, it's safe to just keep a reference
         elif isinstance(bits, int):
-            self._bits = numpy.zeros(bits, bool)
-            self._bits.flags.writeable = False
+            self._bits = numpy.zeros(bits, bool)  # If we're just given a number, treat it as a length and fill with 0s
+            self._bits.flags.writeable = False  # Make sure the bit array isn't writable
         elif isinstance(bits, BitString):
             # No need to make a copy because we use immutable bit arrays
-            self._bits = bits._bits
+            self._bits = bits._bits  # We can just grab a reference to the same bit array the other bitstring is using
         else:
-            self._bits = numpy.array(bits, bool)
-            self._bits.flags.writeable = False
+            self._bits = numpy.array(bits, bool)  # Make a new bit array from the given values
+            self._bits.flags.writeable = False  # Make sure the bit array isn't writable
 
-        self._hash = None
-
-#    @property
-#    def bits(self):
-#        """The numpy array containing the actual bits of the bit string. Note that the array is immutable."""
-#        # Safe because we use immutable bit arrays
-#        return self._bits
+        self._hash = None  # We'll calculate this later if we need it.
 
     def any(self):
         """Returns True iff at least one bit is set."""
@@ -95,12 +101,15 @@ class BitString:
         return int(numpy.count_nonzero(self._bits))
 
     def __str__(self):
+        # Overloads str(bitstring)
         return ''.join('1' if bit else '0' for bit in self._bits)
 
     def __repr__(self):
+        # Overloads repr(bitstring)
         return type(self).__name__ + '(' + repr([int(bit) for bit in self._bits]) + ')'
 
     def __int__(self):
+        # Overloads int(bitstring)
         value = 0
         for bit in self._bits:
             value <<= 1
@@ -108,69 +117,81 @@ class BitString:
         return value
 
     def __len__(self):
+        # Overloads len(bitstring)
         return len(self._bits)
 
     def __iter__(self):
+        # Overloads iter(bitstring), and also, for bit in bitstring
         return iter(self._bits)
 
     def __getitem__(self, index):
+        # Overloads bitstring[index]
         return self._bits[index]
 
     def __hash__(self):
+        # Overloads hash(bitstring)
+        # If the hash value hasn't already been calculated, do so now.
         if self._hash is None:
             self._hash = len(self._bits) + (hash(int(self)) << 13)
         return self._hash
 
     def __eq__(self, other):
+        # Overloads ==
         # noinspection PyProtectedMember
         return isinstance(other, BitString) and numpy.array_equal(self._bits, other._bits)
 
     def __ne__(self, other):
+        # Overloads !=
         return not self == other
 
     def __and__(self, other):
+        # Overloads &
         if isinstance(other, int):
             other = BitString.from_int(other, len(self._bits))
         elif not isinstance(other, BitString):
             other = BitString(other)
         bits = numpy.bitwise_and(self._bits, other._bits)
         # noinspection PyUnresolvedReferences
-        bits.flags.writeable = False
+        bits.flags.writeable = False  # Make sure the bit array isn't writable so it can be used by the constructor
         return type(self)(bits)
 
     def __or__(self, other):
+        # Overloads |
         if isinstance(other, int):
             other = BitString.from_int(other, len(self._bits))
         elif not isinstance(other, BitString):
             other = BitString(other)
         bits = numpy.bitwise_or(self._bits, other._bits)
         # noinspection PyUnresolvedReferences
-        bits.flags.writeable = False
+        bits.flags.writeable = False  # Make sure the bit array isn't writable so it can be used by the constructor
         return type(self)(bits)
 
     def __xor__(self, other):
+        # Overloads ^
         if isinstance(other, int):
             other = BitString.from_int(other, len(self._bits))
         elif not isinstance(other, BitString):
             other = BitString(other)
         bits = numpy.bitwise_xor(self._bits, other._bits)
         # noinspection PyUnresolvedReferences
-        bits.flags.writeable = False
+        bits.flags.writeable = False  # Make sure the bit array isn't writable so it can be used by the constructor
         return type(self)(bits)
 
     def __invert__(self):
+        # Overloads unary ~
         bits = numpy.bitwise_not(self._bits)
         # noinspection PyUnresolvedReferences
-        bits.flags.writeable = False
+        bits.flags.writeable = False  # Make sure the bit array isn't writable so it can be used by the constructor
         return type(self)(bits)
 
     def __add__(self, other):
+        # Overloads +
         if isinstance(other, int):
             other = BitString.from_int(other, len(self._bits))
         elif not isinstance(other, BitString):
             other = BitString(other)
         bits = numpy.concatenate((self._bits, other._bits))
-        bits.flags.writeable = False
+        bits.flags.writeable = False  # Make sure the bit array isn't writable so it can be used by the constructor
         return type(self)(bits)
 
 
