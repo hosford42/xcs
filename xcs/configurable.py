e7c13a5f5194691a027d1ceb5425006aa82fb432
Aaron Hosford <hosford42@gmail.com>
2024-03-26 00:01:58 -0500

    Support for types and datetimes in config

--- xcs/configurable.py
+++ xcs/configurable.py
@@ -1,3 +1,4 @@
+import datetime
 import importlib
 from typing import Any
 
@@ -11,6 +12,44 @@ def get_type(module_name: str, class_name: str) -> type:
     result = getattr(module, class_name)
     if not isinstance(result, type):
         raise TypeError(result)
+    return result
+
+
+def python_type_from_config(config: dict[str, Any]) -> type:
+    assert config['__module__'] == type.__module__
+    assert config['__class__'] == type.__name__
+    return get_type(config['module'], config['name'])
+
+
+def python_type_to_config(type_obj: type) -> dict[str, Any]:
+    assert isinstance(type_obj, type)
+    return dict(
+        __module__=type.__module__,
+        __class__=type.__name__,
+        module=type_obj.__module__,
+        name=type_obj.__name__
+    )
+
+
+DEFAULT_DATETIME_FORMAT = '%Y%m%d%H%M%S.%f'
+
+
+def datetime_from_config(config: dict[str, Any]) -> datetime.datetime:
+    assert config['__module__'] == datetime.datetime.__module__
+    assert config['__class__'] == datetime.datetime.__name__
+    return datetime.datetime.strptime(config['value'], config.get('format', DEFAULT_DATETIME_FORMAT))
+
+
+def datetime_to_config(dt: datetime.datetime, fmt: str = None) -> dict[str, Any]:
+    assert isinstance(dt, datetime.datetime)
+    fmt = fmt or DEFAULT_DATETIME_FORMAT
+    result = dict(
+        __module__=datetime.datetime.__module__,
+        __class__=datetime.datetime.__name__,
+        value=dt.strftime(fmt)
+    )
+    if fmt != DEFAULT_DATETIME_FORMAT:
+        result['format'] = fmt
     return result
 
 
@@ -39,8 +78,7 @@ class Configurable:
             elif isinstance(property_value, Configurable):
                 config[property_name] = property_value.get_configuration()
             elif isinstance(property_value, type):
-                config[property_name] = dict(__module__=type.__module__, __class__=type.__name__,
-                                             module=property_value.__module__, name=property_value.__name__)
+                config[property_name] = python_type_to_config(property_value)
         return config
 
     def configure(self, config: dict[str, Any]) -> None:
@@ -61,9 +99,7 @@ class Configurable:
             elif (isinstance(property_default, type) and
                   isinstance(property_override, dict) and
                   property_override.keys() == {'__module__', '__class__', 'module', 'name'}):
-                module = property_override['module']
-                name = property_override['name']
-                property_override = get_type(module, name)
+                property_override = python_type_from_config(property_override)
                 try:
                     setattr(self, property_name, property_override)
                 except AttributeError:
